1b535eca1c74b203fac83d22dd1c8cd8b47d0fbf
farCompSci <88432392+farCompSci@users.noreply.github.com>
2022-06-29 15:34:29 +0300

    Modified and Corrected

--- factory_car.py
+++ factory_car.py
@@ -13,29 +13,32 @@ class Engine(ABC):
 # TODO 2: Making the Engine implementations
 class CapuletEngine(Engine):
     """Capulet Engine is implementation of the Engine Abstract Class"""
-    last_service_mileage = 0  # some int representing the number of miles since last service
-    current_mileage = 0  # the mileage (int) at which the car currently is at.
+    def __init__(self, current_mileage: int, last_service_mileage: int):
+        self.last_service_mileage = last_service_mileage  # some int representing the number of miles since last service
+        self.current_mileage = current_mileage  # the mileage at which the car currently is at.
 
     def need_service(self) -> bool:
         """Checks if mileage is greater than miles necessary for service, in this case 30_000"""
-        if (CapuletEngine.current_mileage - CapuletEngine.last_service_mileage) >= 30_000:
+        if (self.current_mileage - self.last_service_mileage) > 30_000:
             return True
         return False
 
 class WilloughbyEngine(Engine):
     """Willoughby Engine is implementation of the Engine Abstract Class"""
-    last_service_mileage = 0
-    current_mileage = 0
+    def __init__(self, current_mileage: int, last_service_mileage: int):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
 
     def need_service(self) -> bool:
         """Checks if mileage is greater than miles necessary for service, in this case 60_000"""
-        if (CapuletEngine.current_mileage - CapuletEngine.last_service_mileage) >= 60_000:
+        if (self.current_mileage - self.last_service_mileage) >= 60_000:
             return True
         return False
 
 class SternmanEngine(Engine):
     """Sternman Engine is implementation of the Engine Abstract Class"""
-    warning_indicator_status = False  # In this case warning indicator status determines whether servicing is necessary
+    def __init__(warning_indicator_status: bool):
+        self.warning_indicator_status = warning_indicator_status  # In this case warning indicator status determines whether servicing is necessary
 
     def need_service(self) -> bool:
         """Checks if services are necessary, which happens only if warning indicator is on / denoted by True"""
@@ -54,8 +57,9 @@ class Battery(ABC):
 #TODO 4: Making Battery Implementations
 class SpindlerBattery(Battery):
     """Spindler Battery is an implementation of the Battery implementation"""
-    current_date = date.today().year #Getting current date
-    last_service_date = 2022 #Getting the year of Battery's last service
+    def __init__(self, current_date: int, last_service_date: int):
+        self.current_date = current_date #Getting current date in years
+        self.last_service_date = last_service_date #Getting the year of Battery's last service
 
     def need_service(self) -> bool:
         """Battery needs service if it has not been serviced in 2 years"""
@@ -65,8 +69,9 @@ class SpindlerBattery(Battery):
 
 class NubbinBattery(Battery):
     """Spindler Battery is an implementation of the Battery implementation"""
-    current_date = date.today().year #Getting current date
-    last_service_date = 2022 #Getting the year of Battery's last service
+    def __init__(self, current_date: int, last_service_date: int):
+        self.current_date = current_date #Getting current date in years
+        self.last_service_date = last_service_date #Getting the year of Battery's last service
 
     def need_service(self) -> bool:
         """Battery needs service if it has not been serviced in 4 years"""
@@ -80,7 +85,7 @@ class Serviceable(ABC):
     @abstractmethod
     def need_service(self,engine,battery) -> bool:
         """Makes sure that the implementation will check if the battery and engine are serviceable"""
-
+        pass
 #TODO 6: Making the Car Interface
 class Car(Serviceable):
     """Car Interface"""
@@ -90,7 +95,7 @@ class Car(Serviceable):
         self.battery = Battery
 
     def need_service(self,engine,battery) -> bool:
-        if self.engine.need_service or self.battery.need_service:
+        if self.engine.need_service() or self.battery.need_service():
             return True
         return False
 
@@ -98,39 +103,40 @@ class Car(Serviceable):
 class CarFactory:
     date = date.today().year
 
-    def create_calliope(current_date = date, last_service_date = date, current_mileage = 0, last_service_mileage = 0) -> Car():
-        calliope = Car(CapuletEngine,SpindlerBattery)
-        calliope.battery.last_service_date = last_service_date
-        calliope.engine.last_service_mileage = last_service_mileage
-        calliope.current_mileage = current_mileage
-        return calliope
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = CapuletEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine,battery)
+        return car
+    
+     @staticmethod
+     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine,battery)
+        return car
 
-    def create_glissade(current_date = date, last_service_date = date, current_mileage = 0, last_service_mileage = 0) -> Car():
-        glissade = Car(WilloughbyEngine,SpindlerBattery)
-        glissade.battery.last_service_date = last_service_date
-        glissade.engine.last_service_mileage = last_service_mileage
-        glissade.current_mileage = current_mileage
-        return glissade
+     @staticmethod
+     def create_palindrome(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = SternmanEngine(warning_indicator_status)
+        battery = NubbinBattery(current_date,last_service_date)
+        car = Car(engine,battery)
+        return car
 
-    def create_palindrome(current_date = date, last_service_date = date, warning_light_on = True) -> Car():
-        pallindrome = Car(SternmanEngine,SpindlerBattery)
-        pallindrome.battery.last_service_date = last_service_date
-        pallindrome.engine.warning_indicator_status = warning_light_on
-        return pallindrome
+     @staticmethod
+     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage,last_service_date)
+        battery = NubbinBattery(current_date,last_service_date)
+        car = Car(engine,battery)
+        return car
 
-    def create_rorschach(current_date = date, last_service_date = date, current_mileage = 0, last_service_mileage = 0) -> Car():
-        rorschach = Car(WilloughbyEngine,NubbinBattery)
-        rorschach.battery.last_service_date = last_service_date
-        rorschach.engine.last_service_mileage = last_service_mileage
-        rorschach.current_mileage = current_mileage
-        return rorschach
-
-    def create_thovex(current_date = date, last_service_date = date, current_mileage = 0, last_service_mileage = 0) -> Car():
-        thovex = Car(CapuletEngine,NubbinBattery)
-        thovex.battery.last_service_date = last_service_date
-        thovex.engine.last_service_mileage = last_service_mileage
-        thovex.current_mileage = current_mileage
-        return thovex
+     @staticmethod
+     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = CapuletEngine(current_mileage,last_service_date)
+        battery = NubbinBattery(current_date,last_service_date)
+        car = Car(engine,battery)
+        return car
 
 
 
