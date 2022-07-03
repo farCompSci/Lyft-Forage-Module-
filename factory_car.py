9c276f65da04e0226b6bef5b208710373f9c4c95
farCompSci <88432392+farCompSci@users.noreply.github.com>
2022-07-03 15:33:00 +0300

    Added tires

--- factory_car.py
+++ factory_car.py
@@ -73,8 +73,8 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date  # Getting the year of Battery's last service
 
     def need_service(self) -> bool:
-        """Battery needs service if it has not been serviced in 2 years"""
-        if (self.current_date - self.last_service_date) >= 2:
+        """Battery needs service if it has not been serviced in 3 years"""
+        if (self.current_date - self.last_service_date) >= 3:
             return True
         return False
 
@@ -92,8 +92,39 @@ class NubbinBattery(Battery):
             return True
         return False
 
+#TODO 5: Making the new Tires Interface
+class Tires(ABC):
+    """Adding Tires Interface"""
 
-# TODO 5: Making the Serviceable Interface
+    @abstractmethod
+    def need_service(self) -> bool:
+        """Checking the tires to see if any needs service"""
+
+#TODO 6: Making the Tires Implementations
+class CarriganTires(Tires):
+    def __init__(self,tires_state):
+        self.tires_state = tires_state # Getting the state of the tires in an array
+
+    def need_service(self) -> bool:
+        """Checking the tires array"""
+        for tire in self.tires_state:
+            if tire >= 0.9:
+                return True
+        return False
+
+class OctoprimeTires(Tires):
+    def __init__(self,tires_state):
+        self.tires_state = tires_state #Getting state of the tires
+
+    def need_service(self) -> bool:
+        wear_sum = 0
+        for tire in self.tires_state:
+            wear_sum += tire
+        if wear_sum >= 3:
+            return True
+        return False
+
+# TODO 7: Making the Serviceable Interface
 class Serviceable(ABC):
     """Serviceable Interface, checks if a Car will be serviceable or not"""
 
@@ -103,56 +134,63 @@ class Serviceable(ABC):
         pass
 
 
-# TODO 6: Making the Car Interface
+# TODO 8: Making the Car Interface
 class Car(Serviceable):
     """Car Interface"""
 
-    def __init__(self, Engine, Battery):
+    def __init__(self, Engine, Battery,Tires):
         """Takes Engine and Battery as Constructor Parameters"""
         self.engine = Engine
         self.battery = Battery
+        self.tires = Tires
 
     def need_service(self, engine, battery) -> bool:
-        if self.engine.need_service() or self.battery.need_service():
+        if self.engine.need_service() or self.battery.need_service() or self.tires.need_service():
             return True
         return False
 
 
-# TODO 7: Create Car Factory
+# TODO 9: Create Car Factory
 class CarFactory:
     date = date.today().year
 
     @staticmethod
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage,tire_state) -> Car:
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine, battery)
+        tires = CarriganTires(tire_state)
+        car = Car(engine, battery, tires)
         return car
 
     @staticmethod
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage,tire_state) -> Car:
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine, battery)
+        tires = CarriganTires(tire_state)
+        car = Car(engine, battery, tires)
         return car
 
     @staticmethod
-    def create_palindrome(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+    def create_palindrome(current_date, last_service_date,warning_indicator_status,tire_state) -> Car:
         engine = SternmanEngine(warning_indicator_status)
         battery = NubbinBattery(current_date, last_service_date)
-        car = Car(engine, battery)
+        tires = CarriganTires(tire_state)
+        car = Car(engine, battery, tires)
         return car
 
     @staticmethod
-    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        engine = WilloughbyEngine(current_mileage, last_service_date)
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage,tire_state) -> Car:
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
-        car = Car(engine, battery)
+        tires = OctoprimeTires(tire_state)
+        car = Car(engine, battery, tires)
         return car
 
     @staticmethod
-    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
-        engine = CapuletEngine(current_mileage, last_service_date)
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage,tire_state) -> Car:
+        engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
-        car = Car(engine, battery)
+        tires = OctoprimeTires(tire_state)
+        car = Car(engine, battery, tires)
         return car
+
